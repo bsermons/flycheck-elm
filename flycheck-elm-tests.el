0f277040a2c701a76f6d330b7e5e1dd7f8302c32
bsermons <bsermons@turner-industries>
2015-07-09 15:01:33 -0500

    Fixing headers

--- flycheck-elm-tests.el
+++ flycheck-elm-tests.el
@@ -1,3 +1,33 @@
+;;; flycheck-elm-tests.el --- Flycheck support for the elm language
+
+;; Copyright (c) 2015 Brian Sermons
+
+;; Author: Brian Sermons
+;; Package-Requires: ((flycheck "0.24") (emacs "24.4"))
+;; URL: https://github.com/bsermons/flycheck-elm
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Usage: (eval-after-load 'flycheck
+;;;          '(add-hook 'flycheck-mode-hook #'flycheck-elm-setup))
+
+;;; Code:
+;; Expected error format
 ;; [
 ;;     {
 ;;         "details": "",
@@ -25,19 +55,21 @@
 (require 'flycheck-elm)
 (require 'json)
 
+;; Example errors
 (setq sdata  "[{\"subregion\":null,\"suggestions\":[],\"details\":\"\",\"region\":{\"end\":{\"column\":12,\"line\":3},\"start\":{\"column\":8,\"line\":3}},\"type\":\"error\",\"file\":\"Sample.elm\",\"tag\":\"NAMING ERROR\",\"overview\":\"Cannot find variable `text`\"}]")
 (setq mdata "[{\"details\":\"\",\"region\":{\"end\":{\"column\":40,\"line\":4},\"start\":{\"column\":1,\"line\":4}},\"type\":\"warning\",\"file\":\"g:/projects/gymfuturistic/routine_engine/trunk/FuturisticLabs.RoutineEngine.WebService/scripts/Website.elm\",\"tag\":\"unused import\",\"overview\":\"Module `Html.Attributes` is unused.\"},{\"details\":\"The type annotation you want looks something like this:\\n\\n    init :\\n        { login :\\n              { errors : List a\\n              , login : String\\n              , loginDisabled : Bool\\n              , password : String\\n              , response : Maybe b\\n              , signinAttempts : number\\n              }\\n        }\",\"region\":{\"end\":{\"column\":6,\"line\":28},\"start\":{\"column\":1,\"line\":26}},\"type\":\"warning\",\"file\":\"g:/projects/gymfuturistic/routine_engine/trunk/FuturisticLabs.RoutineEngine.WebService/scripts/Website.elm\",\"tag\":\"missing type annotation\",\"overview\":\"Top-level value `init` does not have a type annotation.\"},{\"details\":\"The type annotation you want looks something like this:\\n\\n    update :\\n        Action\\n        -> { a | login : LoginViewModel } -> { a | login : LoginViewModel }\",\"region\":{\"end\":{\"column\":60,\"line\":34},\"start\":{\"column\":1,\"line\":31}},\"type\":\"warning\",\"file\":\"g:/projects/gymfuturistic/routine_engine/trunk/FuturisticLabs.RoutineEngine.WebService/scripts/Website.elm\",\"tag\":\"missing type annotation\",\"overview\":\"Top-level value `update` does not have a type annotation.\"},{\"details\":\"The type annotation you want looks something like this:\\n\\n    loginSuccessHandler : LoginResponse -> Task a ()\",\"region\":{\"end\":{\"column\":60,\"line\":40},\"start\":{\"column\":1,\"line\":37}},\"type\":\"warning\",\"file\":\"g:/projects/gymfuturistic/routine_engine/trunk/FuturisticLabs.RoutineEngine.WebService/scripts/Website.elm\",\"tag\":\"missing type annotation\",\"overview\":\"Top-level value `loginSuccessHandler` does not have a type annotation.\"},{\"details\":\"The type annotation you want looks something like this:\\n\\n    view :\\n        (a, b)\\n        -> { c |\\n               login :\\n                   { errors : List (String, String)\\n                   , login : String\\n                   , loginDisabled : Bool\\n                   , password : String\\n                   , signinAttempts : Int\\n                   , response : Maybe LoginResponse\\n                   }\\n               }\\n        -> Html\",\"region\":{\"end\":{\"column\":51,\"line\":46},\"start\":{\"column\":1,\"line\":43}},\"type\":\"warning\",\"file\":\"g:/projects/gymfuturistic/routine_engine/trunk/FuturisticLabs.RoutineEngine.WebService/scripts/Website.elm\",\"tag\":\"missing type annotation\",\"overview\":\"Top-level value `view` does not have a type annotation.\"},{\"details\":\"The type annotation you want looks something like this:\\n\\n    model : Signal { login : LoginViewModel }\",\"region\":{\"end\":{\"column\":34,\"line\":50},\"start\":{\"column\":1,\"line\":48}},\"type\":\"warning\",\"file\":\"g:/projects/gymfuturistic/routine_engine/trunk/FuturisticLabs.RoutineEngine.WebService/scripts/Website.elm\",\"tag\":\"missing type annotation\",\"overview\":\"Top-level value `model` does not have a type annotation.\"},{\"details\":\"The type annotation you want looks something like this:\\n\\n    main : a -> b -> Signal Html\",\"region\":{\"end\":{\"column\":45,\"line\":53},\"start\":{\"column\":1,\"line\":52}},\"type\":\"warning\",\"file\":\"g:/projects/gymfuturistic/routine_engine/trunk/FuturisticLabs.RoutineEngine.WebService/scripts/Website.elm\",\"tag\":\"missing type annotation\",\"overview\":\"Top-level value `main` does not have a type annotation.\"}]
 [{\"subregion\":null,\"details\":\"I need an Element, Html, (Signal Element), or (Signal Html) so I can render it\\non screen, but you gave me:\\n\\n    a -> b -> Signal Html\",\"region\":{\"end\":{\"column\":45,\"line\":53},\"start\":{\"column\":1,\"line\":52}},\"type\":\"error\",\"file\":\"g:/projects/gymfuturistic/routine_engine/trunk/FuturisticLabs.RoutineEngine.WebService/scripts/Website.elm\",\"tag\":\"BAD MAIN TYPE\",\"overview\":\"The 'main' value has an unsupported type.\"}]")
 
-(defun ert-test-parse-warning ()
+(defun test-parse-warning ()
   (let* ((json-array-type 'list)
          (data (json-read-from-string "{\"type\":\"warning\"}")))
     (flycheck-elm-decode-type data)))
 
-(defun ert-test-parse-error ()
+(defun test-parse-error ()
   (let* ((json-array-type 'list)
          (data (json-read-from-string "{\"type\":\"error\"}")))
     (flycheck-elm-decode-type data)))
 
 (ert-test-parse-error)
 (elm-parse-error-data mdata)
+;;; flycheck-elm-tests.el ends here
--- flycheck-elm.el
+++ flycheck-elm.el
@@ -1,5 +1,28 @@
 ;;; flycheck-elm.el --- Flycheck support for the elm language
+
+;; Copyright (c) 2015 Brian Sermons
+
+;; Author: Brian Sermons
+;; Package-Requires: ((flycheck "0.24") (emacs "24.4"))
+;; URL: https://github.com/bsermons/flycheck-elm
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
+
 ;;; Usage: (eval-after-load 'flycheck
 ;;;          '(add-hook 'flycheck-mode-hook #'flycheck-elm-setup))
 
@@ -27,7 +50,7 @@
     (pcase type
       (`"warning" 'warning)
       (`"error" 'error)
-      (_ 'warning))))
+      (_ 'unknown))))
 
 (defun flycheck-elm-read-json (str)
   (condition-case nil
